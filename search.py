44f0adafa05528eb954a5ced7b00043be909a6d5
3xsh0re <101967837+REMIXXYH@users.noreply.github.com>
2023-09-08 19:48:37 +0800

    Update search.py
    更新到v0.3

--- search.py
+++ search.py
@@ -3,9 +3,14 @@ import pandas as pd
 import re
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QVBoxLayout, QPushButton, QWidget, QLineEdit, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox, QCheckBox, QFileDialog
 from PyQt5.QtGui import QIcon, QPixmap, QFont
-
 # 读取社工库.xlxs文件
 global data_frame
+# 复选框模式:
+# 000->普通搜索;200->身份证;020->手机号;002->邮箱;
+# 220->身份证+手机号;202->身份证+邮箱;
+# 022->手机号+邮箱;222->身份证+手机号+邮箱
+global mode
+mode = '000'
 
 # 判断是人名还是手机号
 def judge_name_or_phone(search_string):
@@ -30,14 +35,14 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Social-Work-Lib Search ——by 3xsh0re")
-        self.setGeometry(400, 100, 1200, 800)
+        self.setGeometry(200, 100, 1520, 880)
         self.setFixedSize(self.width(), self.height())
 
         # 设置窗口背景图片
         background_image = QPixmap("./src/background.jpeg")
         background_label = QLabel(self)
         background_label.setPixmap(background_image)
-        background_label.setGeometry(0, 0, 1200, 800)
+        background_label.setGeometry(0, 0, 1520, 880)
 
         win_icon = QIcon("./src/title.ico")
         self.setWindowIcon(win_icon)
@@ -85,16 +90,48 @@ class MainWindow(QMainWindow):
         # 将layout和hbox垂直布局
         layout.addLayout(hbox)
 
-        self.checkbox = QCheckBox("功能待开发中")
-        self.checkbox.setStyleSheet("""
+        # 使用水平布局放置复选框
+        hcheckbox = QHBoxLayout()
+        font_checkbox = QFont("楷体", 13)
+
+        self.checkbox_1 = QCheckBox("含有身份证信息")
+        self.checkbox_1.setFont(font_checkbox)
+        self.checkbox_1.setStyleSheet("""
             QCheckBox {
                 color: white;
                 font-family: KaiTi;
             }
         """)
-        layout.addWidget(self.checkbox)
+        hcheckbox.addWidget(self.checkbox_1)
+        self.checkbox_1.stateChanged.connect(lambda: self.btnstate(self.checkbox_1))
 
-        self.file_button = QPushButton("选择文件")
+        self.checkbox_2 = QCheckBox("含有手机号信息")
+        self.checkbox_2.setFont(font_checkbox)
+        self.checkbox_2.setStyleSheet("""
+            QCheckBox {
+                color: white;
+                font-family: KaiTi;
+            }
+        """)
+        hcheckbox.addWidget(self.checkbox_2)
+        self.checkbox_2.stateChanged.connect(lambda: self.btnstate(self.checkbox_2))
+
+        self.checkbox_3 = QCheckBox("含有邮箱信息")
+        self.checkbox_3.setFont(font_checkbox)
+        self.checkbox_3.setStyleSheet("""
+            QCheckBox {
+                color: white;
+                font-family: KaiTi;
+            }
+        """)
+        hcheckbox.addWidget(self.checkbox_3)
+        self.checkbox_3.stateChanged.connect(lambda: self.btnstate(self.checkbox_3))
+
+        # 将layout和hcheckbox垂直布局
+        layout.addLayout(hcheckbox)
+
+        # 设置文件选择按钮
+        self.file_button = QPushButton("选择社工库^_^")
         self.file_button.clicked.connect(self.select_file)
         self.file_button.setFont(font_content)
         self.file_button.setStyleSheet("""
@@ -136,6 +173,7 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+    # 处理搜索出来的内容
     def handle_search(self):
         try:
             # 获取搜索框内容
@@ -168,7 +206,9 @@ class MainWindow(QMainWindow):
             error_dialog.setText("怎么没有选择社工库文件捏？")
             error_dialog.exec_()
 
+    # 搜索函数
     def search(self,search_string):
+        global mode
         # 处理未选择文件的情况
         if len(data_frame) == 0:
             error_dialog = QMessageBox()
@@ -180,31 +220,116 @@ class MainWindow(QMainWindow):
             error_dialog.exec_()
             return 0
         else:
-            # 判断字符串是否合法
-            flag = judge_name_or_phone(search_string)
-            if flag == 1:
-                name_list = data_frame['姓名']
-                # 获取行号
-                results = name_list[name_list==search_string].index
-                # 获取行里的数据
-                row_values = data_frame.iloc[results].values
-                return_results = []
-                for i in range(len(results)):
-                    return_results.append(row_values[i])
-                return return_results
-            elif flag == 2:
-                # 获取手机号列表
-                phone_list = data_frame['手机号']
-                results = phone_list[phone_list==int(search_string)].index
-                # 返回对应行号
-                row_values = data_frame.iloc[results].values
-                return_results = []
-                for i in range(len(results)):
-                    return_results.append(row_values[i])
-                return return_results
-            else:
-                return 0
+            if mode == "000": # 普通搜索
+                # 判断字符串是否合法
+                flag = judge_name_or_phone(search_string)
+                if flag == 1:
+                    name_list = data_frame['姓名']
+                    # 获取行号
+                    results = name_list[name_list==search_string].index
+                    # 获取行里的数据
+                    row_values = data_frame.iloc[results].values
+                    return_results = []
+                    for i in range(len(results)):
+                        return_results.append(row_values[i])
+                    return return_results
+                elif flag == 2:
+                    # 获取手机号列表
+                    phone_list = data_frame['手机号']
+                    results = phone_list[phone_list==int(search_string)].index
+                    # 返回对应行号
+                    row_values = data_frame.iloc[results].values
+                    return_results = []
+                    for i in range(len(results)):
+                        return_results.append(row_values[i])
+                    return return_results
+                else:
+                    return 0
+            else: # 非普通模式
+                if mode == "200": # 身份证
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[2]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "020": # 手机号
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[3]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "002":# 邮箱
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[5]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "220": # 身份证+手机号
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[2]) is not float and type(R[3]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "202": # 身份证+邮箱
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[2]) is not float and type(R[5]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "022": # 手机号+邮箱
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[3]) is not float and type(R[5]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                elif mode == "222": # 身份证+手机号+邮箱
+                    return_results = []
+                    # 遍历 DataFrame 行
+                    for row in data_frame.itertuples():
+                        R = list(row)
+                        # row 是一个命名元组，其中包含了当前行的数据
+                        if type(R[2]) is not float and type(R[3]) is not float and type(R[5]) is not float:
+                            R.pop(0)
+                            return_results.append(R)
+                    return return_results
+                else:
+                    return 0
+                    
+            
     
+    # 复选框状态
+    def btnstate(self, btn):
+        global mode
+        chk1Status = str(self.checkbox_1.checkState())
+        chk2Status = str(self.checkbox_2.checkState())
+        chk3Status = str(self.checkbox_3.checkState())
+        mode = chk1Status+chk2Status+chk3Status
+        print(mode)
+
     # 文件选择函数
     def select_file(self):
         try:
@@ -222,6 +347,8 @@ class MainWindow(QMainWindow):
                 success_dialog.setWindowIcon(icon)
                 success_dialog.setWindowTitle("Success!!!")
                 success_dialog.setText(f"你的社工库路径是：{file_path}")
+                F = file_path.split("/")
+                self.file_button.setText(f"你选择的社工库是：{F[-1]} || 点击我换一个^_^")
                 success_dialog.exec_()
         except Exception as e:
             error_dialog = QMessageBox()
